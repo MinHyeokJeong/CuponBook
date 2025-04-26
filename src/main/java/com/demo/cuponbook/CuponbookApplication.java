f9d737b766d7cf0aae4e3e0354813ca6683718be
MinHyeokJeong <alsgur7839@gmail.com>
2025-04-26 16:46:16 +0900

    first commit

---
+++ src/main/java/com/demo/cuponbook/CuponbookApplication.java
@@ -0,0 +1,13 @@
+package com.demo.cuponbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuponbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuponbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/cuponbook/CuponbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.cuponbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuponbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
