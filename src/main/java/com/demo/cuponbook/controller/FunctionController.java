7c241cb6a325946ebfcf38ae608fd52cd0a06084
MinHyeokJeong <89130071+MinHyeokJeong@users.noreply.github.com>
2025-07-26 15:27:07 +0900

    perf(feature/payment) 쿠폰사용 여부와 결제하기 기능 연계할수 있도록 기능 개선

--- src/main/java/com/demo/cuponbook/controller/FunctionController.java
+++ src/main/java/com/demo/cuponbook/controller/FunctionController.java
@@ -2,22 +2,17 @@ package com.demo.cuponbook.controller;
 
 import com.demo.cuponbook.dto.CouponUseRequest;
 import com.demo.cuponbook.entity.Customer;
-import com.demo.cuponbook.entity.StampLog;
 import com.demo.cuponbook.entity.StampLogInf;
-import com.demo.cuponbook.service.CustomerService;
 import com.demo.cuponbook.service.StampConfirmService;
-import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Controller
 public class FunctionController {
-        private final StampConfirmService stampConfirmService;
+    public boolean couponEvent = false;
+    private final StampConfirmService stampConfirmService;
 
     public FunctionController(StampConfirmService stampConfirmService) {
         this.stampConfirmService = stampConfirmService;
@@ -35,6 +30,12 @@ public class FunctionController {
         int totalStamp = 10;                          // 총 스탬프 개수 (고정 또는 DB에서 가져오기)
         int useableCoupon = customer.getCouponCnt(); // 사용가능한 쿠폰 갯수
 
+        if (couponEvent) {
+            stampCount = stampCount == 0 ? stampCount : stampCount - 1;
+            totalPrice = totalPrice - 1000;
+            iceQty = iceQty - 1;
+        }
+
         model.addAttribute("stampCount", stampCount);
         model.addAttribute("totalStamp", totalStamp);
         model.addAttribute("useableCoupon", useableCoupon);
@@ -51,6 +52,10 @@ public class FunctionController {
     public ResponseEntity<String> handleCouponUse(@RequestBody CouponUseRequest request) {
         try {
             stampConfirmService.useCoupon(request.getPhone());
+            couponEvent = true;
+
+            System.out.println("couponEvent = " + couponEvent);
+
             return ResponseEntity.ok("쿠폰 사용 완료");
         } catch (IllegalStateException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
@@ -59,15 +64,21 @@ public class FunctionController {
 
     @GetMapping("/payment")
     public String costPayment(@RequestParam String phone, Model model) {
+        System.out.println("couponEvent = " + couponEvent);
         Customer customer = stampConfirmService.findCustomerByPhone(phone);
         //StampLog stampLog = stampConfirmService.findByAllCustomer(customer);
         StampLogInf stampLogInf = stampConfirmService.findByCustomerStamp(customer);
 
+        int orderCnt = couponEvent ? stampLogInf.getStampCnt() - 1 : stampLogInf.getStampCnt();
+        int paymentAmount = couponEvent ? stampLogInf.getPaymentAmount() - 1000 : stampLogInf.getPaymentAmount();
+
         model.addAttribute("customer", customer);
         model.addAttribute("stampCnt", customer.getStampCnt());//Custmoer에서 해도되고 stamp로 넘겨도되고
         model.addAttribute("couponCnt", customer.getCouponCnt());
-        model.addAttribute("orderCnt", stampLogInf.getStampCnt());
-        model.addAttribute("paymentAmount", stampLogInf.getPaymentAmount());
+        model.addAttribute("orderCnt", orderCnt);
+        model.addAttribute("paymentAmount", paymentAmount);
+
+        //couponEvent = false;
 
         return "payment";
     }
